ef3294521a014edc574d7dda2e591a23d26b89b0
Daniel Sochor <danielsochor@gmail.com>
2019-09-01 17:19:10 -0500

    file revisions to shorten

--- server.js
+++ server.js
@@ -1,41 +1,22 @@
-//loads environmental variables into process.env global variable injected by node at runtime
-require('dotenv').config();
-require("./config/connection");
-var cors = require('cors')
-
 const express = require("express");
+const cors = require("cors");
 const app = express();
-app.use(cors())
-const path = require("path");
-const env = process.env.NODE_ENV || 'development';
-const reactConfig = require(path.join(__dirname, './config/config.static.json'))[env];
 const PORT = process.env.PORT || 3001;
 
-var logger = require('morgan')
-app.use(logger("dev"));
-
-
 // Define middleware here
-// when set to true, the URL-encoded data will parsed with the qs library instead of the querystring library
 app.use(express.urlencoded({ extended: true }));
-// this is a built-in middleware function in Express. It parses incoming requests with JSON payloads
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../healthy-front-end/build"));
-}
-app.use(express.static(path.join(__dirname, reactConfig))); // serving react files
+app.use(cors());
 
-// Define API routes here
+// Serve up static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Routes
 require("./controllers/routes")(app);
 
-// Send every other request to the React app
-// Define any API routes before this runs
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../healthy-front-end/public/index.html"));
-});
-
-app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
-
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
