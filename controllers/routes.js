1119adfa7973cfccbde3604fdcfdbd18cd8d3b63
Daniel Sochor <danielsochor@gmail.com>
2019-09-08 12:27:09 -0500

    add cors(corsOptionsDelegate) to user login

--- controllers/routes.js
+++ controllers/routes.js
@@ -13,7 +13,7 @@ module.exports = function(app) {
     app.post("/api/user", function(request, response) {
         user.create(request, response);
     });
-    app.post("/api/user/login", function(request, response) {
+    app.post("/api/user/login", cors(corsOptionsDelegate), function(request, response) {
         user.login(request, response);
     });
     app.delete("/api/user/login", function(request, response) {
