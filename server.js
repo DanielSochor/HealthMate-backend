52bfd570db7b45e1f83091f3d8572227792d56f2
Daniel Sochor <danielsochor@gmail.com>
2019-09-09 00:26:24 -0500

    try minimal cors

--- server.js
+++ server.js
@@ -4,16 +4,17 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+//enables pre-flight across the board
 //app.options('*', cors());
 
 app.use(cors());
-app.use((request, response, next) => {
-  response.header('Access-Control-Allow-Origin','*');
-  response.header('Access-Control-Allow-Headers: *', 'Origin, X-Requested-With, Content-Type, Accept');
-  response.header('Access-Control-Allow-Credentials', true);
-  response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
-  next();
-});
+// app.use((request, response, next) => {
+//   response.header('Access-Control-Allow-Origin','*');
+//   response.header('Access-Control-Allow-Headers: *', 'Origin, X-Requested-With, Content-Type, Accept');
+//   response.header('Access-Control-Allow-Credentials', true);
+//   response.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH');
+//   next();
+// });
 
 // var whitelist = ['https://healthy-people-front-end.herokuapp.com/']
 // var corsOptionsDelegate = function (request, callback) {
